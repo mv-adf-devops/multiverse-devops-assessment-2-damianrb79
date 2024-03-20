c8b74e2af89f65a938cf2bcd829bc478bfd9349c
damianrb79 <148861818+damianrb79@users.noreply.github.com>
2024-03-20 14:34:07 +0000

    Ignore blank Lines

--- src/input.py
+++ src/input.py
@@ -5,7 +5,12 @@ def is_duplicate(row, result):
         if row[0] == r[0]:
             return True
     return False
- 
+
+def is_blank_line(row):
+    if not all(row):    
+        return True
+    return False
+
 def read_csv():
     result = []
     with open(os.path.dirname(__file__) + '/../results.csv', newline='') as csvfile:
@@ -13,6 +18,8 @@ def read_csv():
         next(resultsreader)
         for row in resultsreader:
             if is_duplicate(row, result):
-                continue             
+                continue     
+            if is_blank_line(row):
+                continue        
             result.append(row)
     return result 
--- tests/tests.py
+++ tests/tests.py
@@ -8,3 +8,7 @@ def test_read_csv_no_duplicates():
     result = input.read_csv()
     assert len(result) == len(set([x[0] for x in result])), "List has duplicate elements"
 
+def test_read_csv_removes_blank_line():
+    result = input.read_csv()
+    assert all(all(x) for x in result), "List has BLANKS"
+
